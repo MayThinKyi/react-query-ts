4c35e36491e2e9da7eb881dff6603fc142178285
MayThinKyi303 <maythinkyi2003@gmail.com>
2023-12-16 20:33:07 +0630

    added mutations

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -3,22 +3,20 @@ import { Post } from "../types/types";
 
 export async function getAllPosts(){
      const res=await axios.get<Post[]>('http://localhost:3000/posts');
-     const data= res.data;
-     return data;
+     return res.data;
 }
 
 export async function getPost(id:number){
     const res=await axios.get<Post>(`http://localhost:3000/posts/${id}`);
-    const data= res.data;
-    return data;
+    return res.data;
 }
 
 export async function createPost(post:Post){
     const res=await axios.post<Post>('http://localhost:3000/posts',{
        ...post
     });
-    const data= res.data;
-    return data;
+    
+    return res.data;
 }
 export async function deletePost(id:number){
     const res=await axios.delete(`http://localhost:3000/posts/${id}`);
@@ -27,6 +25,5 @@ export async function deletePost(id:number){
 
 export async function updatePost(data:Post){
     const res=await axios.put(`http://localhost:3000/posts/${data.id}`,data);
-    const up=await res.data;
-    return data;
+    return res.data;
 }
